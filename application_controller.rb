57c3bc3e6c1ef82d9bba63f96617c23ea6079ed4
bjsalgado <bayardo_salgado@yahoo.com>
2015-11-21 19:37:41 +0800

    public set in application_controller

--- application_controller.rb
+++ application_controller.rb
@@ -11,6 +11,7 @@ class ApplicationController < Sinatra::Base
 	register Sinatra::Flash
 
 	set :views, File.expand_path('../../views', __FILE__)
+	set :public_folder, File.expand_path('../../public', __FILE__)
 
 	configure do
 		set :session_secret, 'zmz!'
