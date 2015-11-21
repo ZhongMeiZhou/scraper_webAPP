2675545c70fb4c1c6dd8478ff05376b803d57486
bjsalgado <bayardo_salgado@yahoo.com>
2015-11-21 19:20:20 +0800

    application controller and web_helper done

--- application_controller.rb
+++ application_controller.rb
@@ -1,8 +1,72 @@
 require 'sinatra/base'
+require 'sinatra/flash'
+require 'slim'
+require 'json'
+require_relative './helpers/web_helper.rb'
 
 class ApplicationController < Sinatra::Base
+	include WebAppHelper
 
-	get '/' do
-		'scraper_webapp is up and Running'
+	enable :sessions
+	register Sinatra::Flash
+
+	set :views, File.expand_path('../../views', __FILE__)
+
+	configure do
+		set :session_secret, 'zmz!'
+    		set :api_ver, 'api/v1'
 	end
+	
+	configure :production do
+    		set :api_server, 'http://zmztours.herokuapp.com'
+  	end
+
+	configure :production, :development do
+    		enable :logging
+  	end
+
+	# GUI Lambdas
+ 	get_root = lambda do
+    		slim :home
+  	end
+
+	get_tour_search = lambda do
+    		slim :tours
+  	end
+	
+	post_tours = lambda do
+    		request_url = "#{settings.api_server}/#{settings.api_ver}/tours"
+    		country_tour = post_api_tour(params[:tour], request_url)		
+    		if country_tour['status']?
+			session[:results] = country_tour['result']
+    			session[:action] = :create
+			redirect "/tours/#{country_tour['id']}"
+		else			
+      			flash[:notice] = country_tour['message']
+      			redirect "/tours"
+    		end
+  	end
+
+	get_tours = lambda do
+    		if session[:action] == :create
+      			@results = JSON.parse(session[:results])
+    		else
+      			request_url = "#{settings.api_server}/#{settings.api_ver}/tours/#{params[:id]}"
+      			get_api_tours (request_url)
+      			if @results.code != 200
+        			flash[:notice] = "Cannot find any tours for #{params[:country]}"
+        			redirect "/#{settings.api_ver}/tours"
+      			end
+    		end
+    		@country = @results['country'].upcase
+    		@tours = JSON.parse(@results['tours'])
+    		slim :tours
+  	end
+
+
+  	# GUI Routes
+  	get '/', &get_root
+  	get "/tours", &get_tour_search
+  	post "/tours", &post_tours
+  	get '/tours/:id', &get_tours
 end
---
+++ helpers/web_helper.rb
@@ -0,0 +1,38 @@
+require 'httparty'
+require 'json'
+require_relative '../forms/tour_form'
+
+module WebAppHelper
+	
+	def post_api_tour (country, request_url)
+		submit = TourForm.new
+    		submit.country = country
+		if submit.valid? == false
+			{ :status => false, :message => 'You broke it!' }
+    		else
+    			options = {
+      				body: country.to_json,
+      				headers: { 'Content_Type' => 'application/json'}
+    			}
+    			results = HTTParty.post(request_url, options)
+
+    			if (results.code != 200)
+				{ :status => false, :message => "The Pony Express did not deliver the goods." }
+    			else 
+	    			id = results.request.last_uri.path.split('/').last
+				{ :status => true, :result => results.to_json, :id => id }
+			end
+		end
+	end
+
+	def get_api_tours (url)
+      		options = { headers: { 'Content-Type' => 'application/json'}}
+      		begin
+        		@results = HTTParty.get(url,options)
+      		rescue StandardError => e
+        		logger.info e.message
+        		halt 400, e.message
+      		end
+	end
+end
+
