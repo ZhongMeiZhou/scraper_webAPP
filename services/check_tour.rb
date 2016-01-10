8068955975560a48848988116313efe46827f9f7
nweat <nikkiweat@gmail.com>
2016-01-10 22:34:38 +0800

    add log messages

--- services/check_tour.rb
+++ services/check_tour.rb
@@ -19,7 +19,8 @@ class CheckToursFromAPI
     tours_result.filtered_categories = result['data']['filtered_categories']
     tours_result.all_categories = result['data']['all_categories']
     tours_result.tours = result['data']['tours']
-
+    
+    logger = Logger.new(STDOUT)
     logger.info("CHECK RETURNED RESULTS FROM API:")
     logger.info(tours_result)
     tours_result
